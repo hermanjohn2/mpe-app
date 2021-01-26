bc22421a66bb7edc713cb748560e1a533c627eb8
John Herman <hermanjohn2@gmail.com>
2021-01-25 22:29:30 -0500

    Converted previously created HomeCards into a feature that allows users to click on a title and the card will display below. Utilized the useState hook.

--- client/src/components/HomeCard/index.js
+++ client/src/components/HomeCard/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
@@ -8,26 +10,41 @@ import './style.css';
 const HomeCard = props => {
 	const data = props.cardData;
 
+	// Using state of show to set className for card
+	let display;
+	!props.show ? (display = 'hide') : (display = 'home-card');
+
 	return (
-		<Col className="mt-5 mx-1">
-			<Card className="shadow-lg home-card">
-				<Card.Body className="text-center">
-					<Card.Title className="home-card-text">{data.title}</Card.Title>
+		<Container>
+			<Row>
+				<Col className="mt-2 mx-1">
+					<Card className={`shadow-lg ${display}`}>
+						<Card.Body className="text-center">
+							<button
+								className="float-right"
+								onClick={() => props.setShow(false)}>
+								X
+							</button>
+							<Card.Title className="home-card-text">{data.title}</Card.Title>
 
-					{data.id !== 3 ? (
-						<Card.Img className="home-card-img" src={data.image} />
-					) : (
-						<iframe
-							title="Hair Splitting Video"
-							className="home-card-img embed-responsive-item"
-							src={data.image}
-						/>
-					)}
+							{data.id !== 3 ? (
+								<Card.Img className="home-card-img" src={data.image} />
+							) : (
+								<iframe
+									title="Hair Splitting Video"
+									className="home-card-img embed-responsive-item"
+									src={data.image}
+								/>
+							)}
 
-					<Card.Text className="home-card-text mt-2">{data.body}</Card.Text>
-				</Card.Body>
-			</Card>
-		</Col>
+							<Card.Text className="home-card-text mt-2">
+								{data.body}
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Navigation from '../../components/Navigation';
+import HomeCard from '../../components/HomeCard';
+
+import cardData from '../../utils/card-data';
 
 import './style.css';
 
 const Home = () => {
+	const [cardId, setCardId] = useState(0);
+	const [show, setShow] = useState(false);
+
+	const handleModalClick = e => {
+		const chosenElValue = e.target.getAttribute('value');
+
+		setCardId(chosenElValue - 1); // Subtracting 1 to align id with array index
+
+		setShow(true);
+	};
+
 	return (
 		<div>
 			<Navigation />
 
 			<h1 className="text-center">Your personal edge sharpener.</h1>
+			<br />
 			<h2 className="text-center">
-				<span className="modal-trigger" value="1">
+				<span className="modal-trigger" value="1" onClick={handleModalClick}>
 					Small Business
 				</span>{' '}
 				|{' '}
-				<span className="modal-trigger" value="1">
+				<span className="modal-trigger" value="2" onClick={handleModalClick}>
 					Professional Service
 				</span>{' '}
 				|{' '}
-				<span className="modal-trigger" value="1">
+				<span className="modal-trigger" value="3" onClick={handleModalClick}>
 					Expert Results
 				</span>
 			</h2>
+
+			<HomeCard show={show} setShow={setShow} cardData={cardData[cardId]} />
 		</div>
 	);
 };
